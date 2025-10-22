c848aff9adf71afecfb0a5227544d4304fbaa621
Marley Buchman <michaelmbuchman@gmail.com>
2025-10-21 19:37:02 -0700

    adding socket retries parameter fixed a bug with timeouts from cdn

--- pipeline/download_video.sh
+++ pipeline/download_video.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-GAME_DATE="10-12-2025"
+GAME_DATE="10-20-2025"
+
 GAME_VIDEO_START_TIME="0830PM"
 OUTPUT_DEST="game_videos/${GAME_DATE}/${GAME_DATE}_${GAME_VIDEO_START_TIME}_raw.mp4"
-AMLST_FILE="/redirect/a9e135ab2af84f80a37e67dfdb4c4454-vod/amlst:ie1bWBvmqxHbyY1I6ovyYqVox3i33GuId7CKhqmGpUaG7gwPfAfQpKLP_2BoaTbDNX2h26ZwwyoovO_2Bv2oFUs6D_2Bav_2BC16TCfAzoKpL7kmP_2BPgDKJB6lEB9aNcASaGbmBufddc3wQHnY13mM4Xq6fMUgteboqseiZB"
+AMLST_FILE="/redirect/a9e135ab2af84f80a37e67dfdb4c4454-vod/amlst:XZAI9eD_2FEE4eZA41D7bHhtoYLC6DG6lLPPoyc_2FsAAdul67Guf7xFdXIzqV1dvy3_2Fx09A_2BtAb5mDv6nsuEbdlBnEtcK3DFZ5nzCrttMxAAW9NaaofV6tR3Lu2b1DJqGenkgmqc_2BgwpSVilpR6_2BFQ0twFJLr9wTfsw"
 
 URL_FILE="https://cdn.livebarn.com${AMLST_FILE}?type=m3u8"
 
@@ -16,5 +17,6 @@ yt-dlp \
   --add-header "Referer:https://watchapi.livebarn.com/" \
   --retries 10 \
   --fragment-retries 10 \
+  --socket-timeout 60 \
   -o "$OUTPUT_DEST" \
   "$URL_FILE"
